d8436c6470787531acc1699e2145f88f04cea978
TheSKid0110 <83471013+TheSKid0110@users.noreply.github.com>
2024-06-11 23:17:53 +0300

    Added interest timer and bankcut

--- businessbanking/autorun/sh_business_banking.lua
+++ businessbanking/autorun/sh_business_banking.lua
@@ -1,3 +1,8 @@
 BUSINESS_BANKING = BUSINESS_BANKING or {}
 
+-- The interest timer is how often the interest is calculated and added to the player's balance.
+BUSINESS_BANKING.InterestTimer = 60
+-- The interest rate is the percentage of the player's balance that is added to their balance every time the interest timer is triggered.
 BUSINESS_BANKING.InterestRate = 0.05
+-- The bank cut is the percentage of cut the bank takes from the player when they make a purchase.
+BUSINESS_BANKING.BankCut = 0.08
